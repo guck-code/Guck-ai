a553ad8486a9dd6b68af37c8f0e7f3607540a6ce
Gurk_code <Vuthanhdang9392@yahoo.com.vn>
2026-03-16 14:05:34 -0400

    Update gurk.py

--- gurk.py
+++ gurk.py
@@ -1,19 +1,20 @@
 from brain.memory import load_memory, add_memory
 from brain.thinker import think
+import os
 
 memory = load_memory()
 
-print("Gurk AI online")
+print("Gurk AI online 🧠💥")
 
 while True:
-
     user = input("human: ")
 
-    reply = think(user)
+    # Run terminal commands if input is shell command
+    if user.startswith(("cd ", "ls", "pwd")):
+        os.system(user)
+        continue
 
+    reply = think(user, memory)
     print("Gurk:", reply)
 
-    memory = add_memory(memory, {
-        "human": user,
-        "Gurk": reply
-    })
+    memory = add_memory(memory, {"human": user, "Gurk": reply})
